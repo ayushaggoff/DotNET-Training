1e9d4ade26fa0da5e46e9198c0a4211c8c8bb6dd
Ayush Aggarwal <ayusha@successive.com>
2020-02-05 12:20:37 +0530

    Basic Oops Concepts classes, Fields, Methods, Properties

--- ProjectVehicle/Program.cs
+++ ProjectVehicle/Program.cs
@@ -10,6 +10,40 @@ namespace ProjectVehicle
     {
         static void Main(string[] args)
         {
+            int vehicleSpeed = 0;
+            int choice = 0;
+            Vehicle objVehicle = new Vehicle("Cyber");
+
+            while (choice != 4)
+            {
+                Console.WriteLine("Check Max Speed Limit On Road");
+                Console.WriteLine("1:Enter Car Details");
+                Console.WriteLine("2:Enter Speed");
+                Console.WriteLine("3:Stop ");
+                Console.WriteLine("4:Exit");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter Color of your vehicle: ");
+                        objVehicle.Color = Console.ReadLine();
+                        Console.WriteLine("Enter Number of Wheels of your vehicle: ");
+                        objVehicle.NoOfWheels = int.Parse(Console.ReadLine());
+                        objVehicle.Start();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter your speed: ");
+                        vehicleSpeed = int.Parse(Console.ReadLine());
+                        objVehicle.SpeedUp(vehicleSpeed);
+                        break;
+                    case 3:
+                        objVehicle.Stop();
+                        break;
+                    default:
+                        Console.WriteLine("Choose Correct Option");
+                        break;
+                }
+            }
         }
     }
 }
---
+++ ProjectVehicle/Vehicle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectVehicle
+{
+    class Vehicle
+    {
+
+        private string vehicleName;
+        public const int MaxSpeed = 60;
+        private string _vehicleColor;
+        private int _noOfWheels;
+        public Vehicle(string vehicleName)
+        {
+            this.vehicleName = vehicleName;
+        }
+        public string Color
+        {
+            get { return _vehicleColor; }
+            set { _vehicleColor = value; }
+        }
+        public int NoOfWheels
+        {
+            get { return _noOfWheels; }
+            set { _noOfWheels = value; }
+        }
+        public void Start()
+        {
+            Console.WriteLine("Your Vehicle " + vehicleName + " " + _noOfWheels + " of color " + _vehicleColor + " has Started");
+            Console.WriteLine("where Vehicle Name:" + vehicleName + ", Number of Wheels:" + _noOfWheels + ",Color:" + _vehicleColor);
+            Console.ReadKey();
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Your Vehicle " + vehicleName + " " + _noOfWheels + " of color " + _vehicleColor + " has Stoped");
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// SpeedUp method take Vehicle Speed entered by the user
+        /// </summary>
+        /// <param name="vehicleSpeed"></param>
+        public void SpeedUp(int vehicleSpeed)
+        {
+            if (vehicleSpeed > MaxSpeed)
+            {
+                Console.WriteLine("You have crossed the " + MaxSpeed + " limit");
+                Console.WriteLine("Your Vehicle " + vehicleName + " " + _noOfWheels + " of color " + _vehicleColor + "speeds up to " + MaxSpeed);
+                Console.Write(".where Vehicle Name:" + vehicleName + ", Number of Wheels:" + _noOfWheels + ",Color:" + _vehicleColor + " & " + vehicleSpeed + " is vehicle speed");
+            }
+            else
+            {
+                Console.WriteLine("Your " + vehicleSpeed + " is underlimit");
+                Console.WriteLine("Max speed allowed:" + MaxSpeed);
+                Console.WriteLine("Your Vehicle " + vehicleName + " " + _noOfWheels + " of color " + _vehicleColor + "speeds up to " + MaxSpeed);
+                Console.Write(".where Vehicle Name:" + vehicleName + ", Number of Wheels:" + _noOfWheels + ",Color:" + _vehicleColor + " & " + vehicleSpeed + " is vehicle speed");
+            }
+            Console.ReadKey();
+        }
+    }
+}
