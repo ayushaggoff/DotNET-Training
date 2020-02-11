1965e8a7757e0e9dae118dc72240591c1b05b487
Ayush Aggarwal <ayusha@successive.com>
2020-02-11 17:20:18 +0530

    3 problem Ques Remove Space, Sum of number,string of length

--- ProjectVehicle/VehicleMain.cs
+++ ProjectVehicle/VehicleMain.cs
@@ -10,7 +10,63 @@ namespace ProjectProblem
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("#1 Remove the N number of Space");
+            Console.Write("Enter the sentance:");
+            string inputString = Console.ReadLine();
+            inputString= inputString.Trim();
+            int lengthOfInputString = inputString.Length;
+            string words = "";
+            string answer ="\"";
+            int i = 0;
+            int j;
+            while (i < inputString.Length)
+            {
+                if (inputString[i] != 32)
+                {
+                    j = i;
+                    while (inputString[j] != 32 && j < inputString.Length - 1)
+                    {
+                        words = words + inputString[j];
+                        j = j + 1;
+                    }
+                    answer = answer + words + " ";
+                    words = "";
+                    i = j;
+                }            
+                i = i + 1;
+            }
+            answer = answer.Trim();
+            answer = answer + "\"";
+            int temp;
+            Console.WriteLine();
+            Console.Write("After removing extra spaces:"+answer);
+            Console.ReadKey();
+            Console.WriteLine("#2 show the sum of number in a String  ");
+            Console.Write("Enter the sentance:");
+            string inputString2 = Console.ReadLine();
+            int sizeOfString = inputString2.Length;
+            int sum = 0;
+            for (int k = 0; k < sizeOfString ; k++)
+            {
+              if (inputString2[k] >= 48 && inputString2[k] < 58)
+                {
+                     temp = inputString2[k]-48;
+                    sum = sum + temp;
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Sum:" + sum);
+
+            Console.ReadKey();
+            Console.WriteLine("#3 show the string with sum of String length & length (of length of String) ");
+            Console.Write("Enter the sentance:");
+            string inputString1 = Console.ReadLine();
+             sizeOfString = inputString1.Length;
+            string tempSizeOfString = sizeOfString.ToString();
+            int sumOfLength = sizeOfString + tempSizeOfString.Length;
+            Console.WriteLine();
+            Console.Write("Output:"+ inputString1+sumOfLength);
+            Console.ReadKey();
         }
     }
 }
