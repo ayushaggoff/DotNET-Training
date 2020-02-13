18f8831295b82266e0f3ce2747b9e3e5a8892174
Ayush Aggarwal <ayusha@successive.com>
2020-02-13 16:23:51 +0530

    StreamReader,StreamWriter

--- ProjectVehicle/Person.cs
+++ ProjectVehicle/Person.cs
@@ -11,7 +11,7 @@ namespace Project
     {
         static void Main(string[] args)
         {
-            string textPath = @"C:\Users\ayusha\Documents\test1";
+            string textPath = @"C:\Users\try\Documents\test1";
             FileStream fs = File.Open(textPath,FileMode.Create);
             string randString = "This is a ramdom string";
             byte[] rsByteArray = Encoding.Default.GetBytes(randString);
@@ -24,6 +24,17 @@ namespace Project
             }
             Console.WriteLine(Encoding.Default.GetString(fileByteArray));
             fs.Close();
+            string textPath1 = @"C:\Users\try\Documents\test2";
+            StreamWriter sw = File.CreateText(textPath1);
+            sw.Write("THis a random text 2 ");
+            sw.WriteLine("Sentence 1");
+            sw.WriteLine("Sentence 2");
+            sw.Close();
+            StreamReader sr = File.OpenText(textPath1);
+            Console.WriteLine("Peek:{0}", Convert.ToChar(sr.Peek()));
+            Console.WriteLine("1st String:{0}", sr.ReadLine());
+            Console.WriteLine("Everything:{0}", sr.ReadToEnd());
+            sr.Close();
             Console.ReadKey();
         }
     }
