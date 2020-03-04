fffb112acd8d19e1a873c4587efeca9bcaca1f25
Ayush Aggarwal <ayusha@successive.com>
2020-03-04 19:17:03 +0530

    task given by Anand Sir :insert ,update,view (table )

---
+++ Person.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DetailsTask
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime Dob { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+
+        public int Insert()
+        {
+            int flag = 0;
+            SqlConnection con = new SqlConnection("data source=LAPR258\\SQLEXPRESS;integrated security=false;Initial Catalog=TestDb;User ID=sa;Password=Successive");
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into tblPersonTask (Name,Gender,DateOfBirth,MaritalStatus,Address,State,Email,Mobile) values(@Name,@Gender,@Dob,@MaritalStatus,@Address,@State,@Email,@Mobile)", con);
+                cmd.Parameters.AddWithValue("@Name", Name);
+                cmd.Parameters.AddWithValue("@Gender", Gender);
+                cmd.Parameters.AddWithValue("@Dob", Dob);
+                cmd.Parameters.AddWithValue("@MaritalStatus", MaritalStatus);
+                cmd.Parameters.AddWithValue("@Address", Address);
+                cmd.Parameters.AddWithValue("@State", State);
+                cmd.Parameters.AddWithValue("@Email", Email);
+                cmd.Parameters.AddWithValue("@Mobile", Mobile);
+                cmd.ExecuteNonQuery();
+                flag = 1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+            finally
+            {
+                con.Close();
+            }
+            return flag;
+        }
+
+        public bool CheckTable(string choiceUpdate)
+        {
+            bool exists = false;
+            SqlConnection con = new SqlConnection("data source=LAPR258\\SQLEXPRESS;integrated security=false;Initial Catalog=TestDb;User ID=sa;Password=Successive");
+            try
+            {
+                con.Open();
+                if (choiceUpdate == "mobile")
+                {
+                    SqlCommand cmd = new SqlCommand("select Email from tblPersonTask where Mobile=@mobile", con);
+                    cmd.Parameters.AddWithValue("@mobile", Mobile);
+                    SqlDataReader read = cmd.ExecuteReader();
+                    if (read.HasRows)
+                    {
+                        exists = true;
+                    }
+                }
+                else
+                {
+                    SqlCommand cmd = new SqlCommand("select Mobile from tblPersonTask where Email=@Email", con);
+                    cmd.Parameters.AddWithValue("@Email", Email);
+                     SqlDataReader read = cmd.ExecuteReader();
+                    if (read.HasRows)
+                    {
+                        exists = true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                exists = false;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return exists;
+        }
+        public int Update(string choice)
+        {
+            int flag = 0;
+            SqlConnection con = new SqlConnection("data source=LAPR258\\SQLEXPRESS;integrated security=false;Initial Catalog=TestDb;User ID=sa;Password=Successive");
+            try
+            {
+                con.Open();
+                if (choice == "mobile")
+                {
+                    SqlCommand cmd = new SqlCommand("update tblPersonTask set Name=@Name,Gender=@Gender,DateOfBirth=@Dob,MaritalStatus=@MaritalStatus,Address=@Address,State=@State where Mobile=@Mobile", con);
+                    cmd.Parameters.AddWithValue("@Mobile", Mobile);
+                    cmd.Parameters.AddWithValue("@Name", Name);
+                    cmd.Parameters.AddWithValue("@Gender", Gender);
+                    cmd.Parameters.AddWithValue("@Dob", Dob);
+                    cmd.Parameters.AddWithValue("@MaritalStatus", MaritalStatus);
+                    cmd.Parameters.AddWithValue("@Address", Address);
+                    cmd.Parameters.AddWithValue("@State", State);
+                    cmd.ExecuteNonQuery();
+                    flag = 1;
+                }
+                else
+                {
+                    SqlCommand cmd = new SqlCommand("update tblPersonTask set Name=@Name,Gender=@Gender,DateOfBirth=@Dob,MaritalStatus=@MaritalStatus,Address=@Address,State=@State where Email=@Email", con);
+                    cmd.Parameters.AddWithValue("@Email", Email);
+                    cmd.Parameters.AddWithValue("@Name", Name);
+                    cmd.Parameters.AddWithValue("@Gender", Gender);
+                    cmd.Parameters.AddWithValue("@Dob", Dob);
+                    cmd.Parameters.AddWithValue("@MaritalStatus", MaritalStatus);
+                    cmd.Parameters.AddWithValue("@Address", Address);
+                    cmd.Parameters.AddWithValue("@State", State);
+                    cmd.ExecuteNonQuery();
+                    flag = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+            finally
+            {
+                con.Close();
+            }
+            return flag;
+        }
+        public void View(string choiceUpdate)
+        {
+            SqlConnection con = new SqlConnection("data source=LAPR258\\SQLEXPRESS;integrated security=false;Initial Catalog=TestDb;User ID=sa;Password=Successive");
+            try
+            {
+                con.Open();
+                if (choiceUpdate == "mobile")
+                {
+                    SqlCommand cmd = new SqlCommand("select * from vwtblPersonTask_GetDetail where Mobile=@Mobile", con);
+                    cmd.Parameters.AddWithValue("@Mobile", Mobile);
+                    SqlDataReader dataReader = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(dataReader);
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        Console.WriteLine("Name:" + row["Name"]);
+                        Console.WriteLine("Date of birth:" + row["DateOfBirth"]);
+                        Console.WriteLine("Gender:" + row["Gender"]);
+                        Console.WriteLine("Email:" + row["Email"]);
+                    }
+                    dataReader.Close();
+                }
+                else
+                {
+                    SqlCommand cmd = new SqlCommand("select * from vwtblPersonTask_GetDetail where Email=@Email", con);
+                    cmd.Parameters.AddWithValue("@Email", Email);
+                    SqlDataReader dataReader = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(dataReader);
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        Console.WriteLine("Name:"+row["Name"]);
+                        Console.WriteLine("Date of birth:" + row["DateOfBirth"]);
+                        Console.WriteLine("Gender:" + row["Gender"]);
+                        Console.WriteLine("Mobile:" + row["Molbile"]);
+                    }
+                    dataReader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+            finally
+            {
+                con.Close();
+            }
+           
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DetailsTask
+{
+    class Program:Person
+    {   
+               
+        static void Main(string[] args)
+        {
+            int flag = 0;
+            //Create Table tblPersonTask
+            //SqlConnection con = new SqlConnection("data source=LAPR258\\SQLEXPRESS;integrated security=false;Initial Catalog=TestDb;User ID=sa;Password=Successive");
+            //try
+            //{
+            //    SqlCommand cmd = new SqlCommand("CREATE TABLE tblPersonTask(Name varchar(150),Gender varchar(150),DateOfBirth datetime,MaritalStatus varchar(150),Address varchar(150),State varchar(150),Email varchar(150) unique,Mobile varchar(20) primary key);", con);
+            //    con.Open();
+            //    cmd.ExecuteNonQuery();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex);
+            //    Console.ReadKey();
+            //}
+            //finally
+            //{
+            //    con.Close();
+            //}
+
+
+
+            //Insert data
+            Person objPerson = new Person();
+            Console.WriteLine("Registration");
+            Console.WriteLine("Enter the following the details");
+            Console.Write("Name: ");
+            objPerson.Name = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Gender: ");
+            objPerson.Gender = Console.ReadLine();
+            Console.WriteLine();
+            Date:
+            Console.Write("Date Of Birth: ");
+            string line = Console.ReadLine();
+            Console.WriteLine();
+            DateTime dt;
+            while (!DateTime.TryParseExact(line, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dt))
+            {
+                Console.WriteLine("Invalid date, please retry");
+
+                goto Date;
+            }
+            objPerson.Dob = dt;
+            Console.Write("Marital Status: ");
+            objPerson.MaritalStatus = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Address: ");
+            objPerson.Address = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("State: ");
+            objPerson.State = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Email ID: ");
+            objPerson.Email = Console.ReadLine();
+            Console.WriteLine();
+            Mobile:
+            Console.Write("Mobile No.: ");
+            objPerson.Mobile = Console.ReadLine();
+            Console.WriteLine();
+            string temp = objPerson.Mobile;
+            if (temp.Length != 10)
+            {
+                Console.WriteLine("Provide the correct input");
+                goto Mobile;
+            }
+            flag = objPerson.Insert();
+            if (flag == 1)
+            {
+                Console.WriteLine("Data Inserted");
+            }
+            else
+            {
+                Console.WriteLine("Data is NOT Inserted");
+            }
+
+
+            //Update Details
+            //Person objPerson = new Person();
+            //Console.WriteLine("Update Details");
+            //string choice = null;
+            //EnterChoice:
+            //Console.Write("Do you want to enter email or mobile:");
+            //string choiceUpdate = Console.ReadLine();
+            //Console.WriteLine();
+            //choiceUpdate = choiceUpdate.ToLower();
+
+            //if (choiceUpdate == "mobile" || choiceUpdate == "email")
+            //{
+
+            //    if (choiceUpdate == "mobile")
+            //    {
+            //        //CheckMobile:
+            //        Console.WriteLine("Enter the mobile number:");
+            //        objPerson.Mobile = Console.ReadLine();
+            //        bool checktbl = objPerson.CheckTable(choiceUpdate);
+            //        if (checktbl == false)
+            //        {
+            //            Console.WriteLine("!! Data do NOT exists");
+            //            // goto CheckMobile;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Enter the following the details");
+            //            Console.Write("Name: ");
+            //            objPerson.Name = Console.ReadLine();
+            //            Console.WriteLine();
+            //            Console.Write("Gender: ");
+            //            objPerson.Gender = Console.ReadLine();
+            //            Console.WriteLine();
+            //            Date:
+            //            Console.Write("Date Of Birth: ");
+            //            string line = Console.ReadLine();
+            //            Console.WriteLine();
+            //            DateTime dt;
+            //            while (!DateTime.TryParseExact(line, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dt))
+            //            {
+            //                Console.WriteLine("Invalid date, please retry");
+            //                goto Date;
+            //            }
+            //            objPerson.Dob = dt;
+            //            Console.Write("Marital Status: ");
+            //            objPerson.MaritalStatus = Console.ReadLine();
+            //            Console.WriteLine();
+            //            Console.Write("Address: ");
+            //            objPerson.Address = Console.ReadLine();
+            //            Console.WriteLine();
+            //            Console.Write("State: ");
+            //            objPerson.State = Console.ReadLine();
+            //            Console.WriteLine();
+            //            flag = objPerson.Update(choiceUpdate);
+            //            if (flag == 1)
+            //            {
+            //                Console.WriteLine("Data Is Updated");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("Data is NOT Updated");
+            //            }
+
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Enter the Email ID:");
+            //        objPerson.Email = Console.ReadLine();
+            //        bool checktbl = objPerson.CheckTable(choiceUpdate);
+            //        if (checktbl == false)
+            //        {
+            //            Console.WriteLine("!! Data do NOT exists");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Enter the following the details");
+            //            Console.Write("Name: ");
+            //            objPerson.Name = Console.ReadLine();
+            //            Console.WriteLine();
+            //            Console.Write("Gender: ");
+            //            objPerson.Gender = Console.ReadLine();
+            //            Console.WriteLine();
+            //            Date:
+            //            Console.Write("Date Of Birth: ");
+            //            string line = Console.ReadLine();
+            //            Console.WriteLine();
+            //            DateTime dt;
+            //            while (!DateTime.TryParseExact(line, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dt))
+            //            {
+            //                Console.WriteLine("Invalid date, please retry");
+            //                goto Date;
+            //            }
+            //            objPerson.Dob = dt;
+            //            Console.Write("Marital Status: ");
+            //            objPerson.MaritalStatus = Console.ReadLine();
+            //            Console.WriteLine();
+            //            Console.Write("Address: ");
+            //            objPerson.Address = Console.ReadLine();
+            //            Console.WriteLine();
+            //            Console.Write("State: ");
+            //            objPerson.State = Console.ReadLine();
+            //            Console.WriteLine();
+            //            flag = objPerson.Update(choiceUpdate);
+            //            if (flag == 1)
+            //            {
+            //                Console.WriteLine("Data Is Updated");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("Data is NOT Updated");
+            //            }
+
+            //        }
+
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("!! Provide the correct input");
+            //    goto EnterChoice;
+            //}
+
+
+
+            //View data
+            //person objperson = new person();
+            //console.writeline("view data");
+            //enterchoice:
+            //console.write("do you want to enter email or mobile:");
+            //string choiceupdate = console.readline();
+            //console.writeline();
+            //choiceupdate = choiceupdate.tolower();
+
+            //if (choiceupdate == "mobile" || choiceupdate == "email")
+            //{
+
+            //    if (choiceupdate == "mobile")
+            //    {
+            //        console.writeline("enter mobile number:");
+            //        objperson.mobile = console.readline();
+            //        bool checktbl = objperson.checktable(choiceupdate);
+            //        if (checktbl == false)
+            //        {
+            //            console.writeline("!! data do not exists");
+            //        }
+            //        else
+            //        {
+            //            objperson.view(choiceupdate);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        console.writeline("enter email id:");
+            //        objperson.email = console.readline();
+            //        bool checktbl = objperson.checktable(choiceupdate);
+            //        if (checktbl == false)
+            //        {
+            //            console.writeline("!! data do not exists");
+            //        }
+            //        else
+            //        {
+            //            objperson.view(choiceupdate);
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    console.writeline("!! provide the correct input");
+            //    goto enterchoice;
+            //}
+            Console.ReadKey();
+        }
+    }
+}
