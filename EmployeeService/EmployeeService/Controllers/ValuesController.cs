9d70a853c0492df182e510b077e89a8816992bbf
Ayush Aggarwal <ayusha@successive.com>
2020-03-20 21:11:04 +0530

    CRUD operation( Alter in ValuesController & Employee)

--- EmployeeService/EmployeeService/Controllers/ValuesController.cs
+++ EmployeeService/EmployeeService/Controllers/ValuesController.cs
@@ -4,17 +4,39 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace EmployeeService.Controllers
 {
     public class ValuesController : ApiController
     {
+        EmployeeService.API.Db.Employee objEmployee = new EmployeeService.API.Db.Employee();
         // GET api/values
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
+        public DataSet GetRecord(int id)
+        {
+            DataSet ds = objEmployee.GetRecordById(id);
+            return ds;
+        }
+
+        public void InsertRecord(API.Db.Employee emp)
+        {
+            objEmployee.InsertRecord(emp);
+        }
+
+        public void UpdateSalaryRecord(API.Db.Employee emp)
+        {
+            objEmployee.UdateSalaryById(emp);
+        }
+        public void DeleteRecord(int id)
+        {
+            objEmployee.DeleteRecordById(id);
+        }
         // GET api/values/5
         public string Get(int id)
         {
---
+++ EmployeeService/EmployeeService/Db/Employee.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace EmployeeService.API.Db
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary{ get; set; }
+        public string Gender { get; set; }
+
+        SqlConnection con = new SqlConnection("data source=LAPR258\\SQLEXPRESS;integrated security=false;Initial Catalog=EmployeeDB;User ID=sa;Password=Successive");
+        public DataSet GetRecordById(int id)
+        {
+            DataSet ds = new DataSet();
+            try {
+                con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Employees where ID=@ID", con);
+            cmd.Parameters.AddWithValue("@ID", id);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            
+            da.Fill(ds);
+        }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: "+ex);
+            }
+            finally 
+            {
+                con.Close();
+            }
+            return ds;
+        }
+
+        public void DeleteRecordById(int id)
+        {
+            try {
+                con.Open();
+            SqlCommand cmd = new SqlCommand("Delete from Employees where ID=@ID", con);
+            cmd.Parameters.AddWithValue("@ID", id);
+            cmd.ExecuteNonQuery();
+        }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: "+ex);
+            }
+            finally 
+            {
+                con.Close();
+            }
+        }
+        public void InsertRecord(Employee emp)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into Employees (FirstName,LastName,Gender,Salary) values(@FirstName,@LastName,@Gender,@Salary)", con);
+                cmd.Parameters.AddWithValue("@FirstName",emp.FirstName);
+                cmd.Parameters.AddWithValue("@LastName",emp.LastName);
+                cmd.Parameters.AddWithValue("@Gender",emp.Gender);
+                cmd.Parameters.AddWithValue("@Salary",emp.Salary);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: "+ex);
+            }
+            finally 
+            {
+                con.Close();
+            }
+        }
+
+        public void UdateSalaryById(Employee emp)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("update Employees set Salary=@Salary where ID=@Id", con);
+                cmd.Parameters.AddWithValue("@id",emp.Id);
+                cmd.Parameters.AddWithValue("@Salary",emp.Salary);
+                 cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: " + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+
+}
