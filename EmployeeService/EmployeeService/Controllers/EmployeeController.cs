5bcf2b934909d2b5492540713bdce68e8beaa4fc
Ayush Aggarwal <ayusha@successive.com>
2020-03-23 20:15:01 +0530

    Created a filter for checking Record is present or not

--- EmployeeService/EmployeeService/Controllers/EmployeeController.cs
+++ EmployeeService/EmployeeService/Controllers/EmployeeController.cs
@@ -1,23 +1,21 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
 using System.Web.Http;
+using System.Web.Http.Controllers;
 using System.Web.Mvc;
 
 namespace EmployeeService.Controllers
-{
-    public class EmployeeController : ApiController
-    {
-        EmployeeService.API.Db.Employee objEmployee = new EmployeeService.API.Db.Employee();
-
-        public DataSet GetRecord(int id)
+{           
+        public class EmployeeController : ApiController
         {
-            DataSet ds = objEmployee.GetRecordById(id);
-            return ds;
-        }
+        EmployeeService.API.Db.Employee objEmployee = new EmployeeService.API.Db.Employee();
         public void InsertRecord(API.Db.Employee emp)
         {
             objEmployee.InsertRecord(emp);
         }
-        public void PutSalaryRecord(API.Db.Employee emp)
+        public void SalaryRecord(API.Db.Employee emp)
         {
             objEmployee.UdateSalaryById(emp);
         }
---
+++ EmployeeService/EmployeeService/Controllers/FilterController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace EmployeeService.Controllers
+{
+    public class CheckRecord : ActionFilterAttribute
+    {
+        EmployeeService.API.Db.Employee objEmployee = new EmployeeService.API.Db.Employee();
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+
+            bool exists = false;
+            SqlConnection con = new SqlConnection("data source=LAPR258\\SQLEXPRESS;integrated security=false;Initial Catalog=TestDb;User ID=sa;Password=Successive");
+            try
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("select ID from Employees  where ID=@id", con);
+                cmd.Parameters.AddWithValue("@id", objEmployee.Id);
+                SqlDataReader read = cmd.ExecuteReader();
+                if (read.HasRows)
+                {
+                    exists = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+            if (exists)
+                HttpContext.Current.Response.Write("Record is Present");
+            else
+                HttpContext.Current.Response.Write("Record is NOT Present");
+        }
+
+        public class FilterController : ApiController
+        {
+            EmployeeService.API.Db.Employee objEmployee = new EmployeeService.API.Db.Employee();
+            [Route("api/Employees")]
+            [CheckRecord]
+            public DataSet GetRecord(int id)
+            {
+                DataSet ds = objEmployee.GetRecordById(id);
+                return ds;
+            }
+
+        }
+    }
+}
