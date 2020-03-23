b9ab380803e4ec63598970ed581bb7eefc512bbd
Ayush Aggarwal <ayusha@successive.com>
2020-03-23 14:27:59 +0530

    Added Employee Controller

---
+++ EmployeeService/EmployeeService/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        EmployeeService.API.Db.Employee objEmployee = new EmployeeService.API.Db.Employee();
+
+        public DataSet GetRecord(int id)
+        {
+            DataSet ds = objEmployee.GetRecordById(id);
+            return ds;
+        }
+        public void InsertRecord(API.Db.Employee emp)
+        {
+            objEmployee.InsertRecord(emp);
+        }
+        public void PutSalaryRecord(API.Db.Employee emp)
+        {
+            objEmployee.UdateSalaryById(emp);
+        }
+        public void DeleteRecord(int id)
+        {
+            objEmployee.DeleteRecordById(id);
+        }
+        // GET: Employee
+
+    }
+}
--- EmployeeService/EmployeeService/Controllers/ValuesController.cs
+++ EmployeeService/EmployeeService/Controllers/ValuesController.cs
@@ -11,48 +11,24 @@ namespace EmployeeService.Controllers
 {
     public class ValuesController : ApiController
     {
-        EmployeeService.API.Db.Employee objEmployee = new EmployeeService.API.Db.Employee();
         // GET api/values
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
-        }
-
-        public DataSet GetRecord(int id)
-        {
-            DataSet ds = objEmployee.GetRecordById(id);
-            return ds;
-        }
-
-        public void InsertRecord(API.Db.Employee emp)
-        {
-            objEmployee.InsertRecord(emp);
-        }
-
-        public void PutSalaryRecord(API.Db.Employee emp)
-        {
-            objEmployee.UdateSalaryById(emp);
-        }
-        public void DeleteRecord(int id)
-        {
-            objEmployee.DeleteRecordById(id);
-        }
+        }      
         // GET api/values/5
         public string Get(int id)
         {
             return "value";
         }
-
         // POST api/values
         public void Post([FromBody]string value)
         {
         }
-
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
         }
-
         // DELETE api/values/5
         public void Delete(int id)
         {
